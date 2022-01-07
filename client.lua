495c8f92c446da4bec2e3040faa5c951bdd628a9
6locc 6a6y <65462366+JellyJamm@users.noreply.github.com>
2022-01-07 12:04:08 -0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,8 @@
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(5)
-		if IsControlPressed(0, 25)
-			then DisableControlAction(0, 22, true)
+		if IsPedArmed(GetPlayerPed(-1), 4 | 2) then 
+			DisableControlAction(0, 22, true)
 		end
 	end
 end)
