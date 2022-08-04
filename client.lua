e20168dbff30c65ab890dda1fc43c718bf95e105
Ziggy <68012775+Ziggy434@users.noreply.github.com>
2022-08-04 22:12:15 +0800

    Ensures that control is only disabled when aiming

--- client.lua
+++ client.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(5)
-		if IsPedArmed(GetPlayerPed(-1), 4 | 2) then 
+		Citizen.Wait(0)
+		if IsPedArmed(GetPlayerPed(-1), 4 | 2) and IsControlPressed(0, 25) then
 			DisableControlAction(0, 22, true)
 		end
 	end
