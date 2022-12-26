79b4cee86d1eba79fa7e84d5245e6a536a30e139
SPKReality <103054717+SPKReality@users.noreply.github.com>
2022-12-26 20:51:01 +0000

    Optimise Code

--- client.lua
+++ client.lua
@@ -1,8 +1,8 @@
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		if IsPedArmed(GetPlayerPed(-1), 4 | 2) and IsControlPressed(0, 25) then
-			DisableControlAction(0, 22, true)
-		end
-	end
+CreateThread(function()
+    while true do
+        Wait(0)
+        if IsPedArmed(PlayerPedId(), 4 | 2) and IsControlPressed(0, 25) then
+            DisableControlAction(0, 22, true)
+        end
+    end
 end)
